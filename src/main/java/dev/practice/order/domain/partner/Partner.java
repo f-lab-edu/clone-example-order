05b22ec4533ee55f8e1d0a66228f9517b033b441
jiunkoo <jiunkoo0106@gmail.com>
2023-09-19 22:26:11 +0900

    partner entity 리팩토링

--- src/main/java/dev/practice/order/domain/partner/Partner.java
+++ src/main/java/dev/practice/order/domain/partner/Partner.java
@@ -1,20 +1,22 @@
 package dev.practice.order.domain.partner;
 
+import dev.practice.order.common.util.TokenGenerator;
+import dev.practice.order.domain.AbstractEntity;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
-import java.time.ZonedDateTime;
+import org.apache.commons.lang3.StringUtils;
 
 @Slf4j
 @Getter
 @Entity
 @NoArgsConstructor
 @Table(name = "partners")
-public class Partner {
+public class Partner extends AbstractEntity {
+    private static final String PREFIX_PARTNER = "ptn_";
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,9 +25,6 @@ public class Partner {
     private String partnerName;
     private String businessNo;
     private String email;
-
-    private ZonedDateTime createdAt;
-    private ZonedDateTime updatedAt;
 
     @Enumerated(EnumType.STRING)
     private Status status;
@@ -39,17 +38,15 @@ public class Partner {
 
     @Builder
     public Partner(String partnerName, String businessNo, String email) {
-        if(partnerName == null) throw new RuntimeException("empty partnerName");
-        if(businessNo == null) throw new RuntimeException("empty businessNo");
-        if(email == null) throw new RuntimeException("empty email");
+        if(StringUtils.isEmpty(partnerName)) throw new RuntimeException("empty partnerName");
+        if(StringUtils.isEmpty(businessNo)) throw new RuntimeException("empty businessNo");
+        if(StringUtils.isEmpty(email)) throw new RuntimeException("empty email");
 
-        this.partnerToken = "abcde";
+        this.partnerToken = TokenGenerator.randomCharacterWithPrefix(PREFIX_PARTNER);
         this.partnerName = partnerName;
         this.businessNo = businessNo;
         this.email = email;
         this.status = Status.ENABLE;
-        this.createdAt = ZonedDateTime.now();
-        this.updatedAt = ZonedDateTime.now();
     }
 
     public void enable() {
