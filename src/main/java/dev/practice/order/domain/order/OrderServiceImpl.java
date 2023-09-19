539402c4031d56122fe7f35765746b946af8085f
jiunkoo <jiunkoo0106@gmail.com>
2023-09-20 08:25:10 +0900

    order service 구현

---
+++ src/main/java/dev/practice/order/domain/order/OrderServiceImpl.java
@@ -0,0 +1,42 @@
+package dev.practice.order.domain.order;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class OrderServiceImpl implements OrderService {
+    private final OrderStore orderStore;
+    private final OrderReader orderReader;
+    private final OrderItemSeriesFactory orderItemSeriesFactory;
+    private final PaymentProcessor paymentProcessor;
+    private final OrderInfoMapper orderInfoMapper;
+
+    @Override
+    @Transactional
+    public String registerOrder(OrderCommand.RegisterOrder command) {
+        Order order = orderStore.store(command.toEntity());
+        orderItemSeriesFactory.store(command, order);
+        return order.getOrderToken();
+    }
+
+    @Override
+    @Transactional
+    public void paymentOrder(OrderCommand.PaymentRequest paymentRequest) {
+        var orderToken = paymentRequest.getOrderToken();
+        var order = orderReader.getOrder(orderToken);
+        paymentProcessor.pay(order, paymentRequest);
+        order.orderComplete();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public OrderInfo.Main retrieveOrder(String orderToken) {
+        var order = orderReader.getOrder(orderToken);
+        var orderItemList = order.getOrderItemList();
+        return orderInfoMapper.of(order, orderItemList);
+    }
+}
